c73230eb567f86884f9d82b0e686554a5e2386ae
pooja-singh-6755 <poojarathore6755@gmail.com>
2026-07-28 02:12:50 +0530

    new changes api

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Force Node to use public DNS servers to resolve Atlas SRV records
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
